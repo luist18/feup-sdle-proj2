ac5ff0da7e1f48f076a6ded6415b691b331b5e24
Henrique Pereira <henriquemrp2001@gmail.com>
2021-12-30 17:01:20 +0000

    Add: commented code

--- modules/src/controller/peer.controller.js
+++ modules/src/controller/peer.controller.js
@@ -11,9 +11,10 @@ export async function status(req, res) {
 export async function start(req, res) {
     const peer = client.peer
 
-    // todo: missing validation
+    // TODO: missing validation
     const { inviteToken } = req.body
 
+    // Checks if invited
     if (inviteToken)
         await peer.start(inviteToken)
     else
@@ -23,22 +24,24 @@ export async function start(req, res) {
 }
 
 export async function stop(req, res) {
-    // todo: missing validation
+    // TODO: missing validation
     throw new Error('Not implemented')
 }
 
 export async function subscribe(req, res) {
     const peer = client.peer
     
+    // Subscription through usernames
     const { username } = req.body
-   
-    if (peer.status !== "online")
-        return res.status(404).json({ "error": "You are not online" })
+
+    // Validation
+    if (peer.status === "offline")
+        return res.status(404).json({ "error": "You are offline" })
 
     if (username === undefined)
         return res.status(400).json({ "error": "Username not provided" })
 
-    // todo: check if user is in the network (works if offline, doesn't work if inexistent)
+    // TODO: check if user is in the network (works if offline, doesn't work if inexistent)
 
     await peer.subscribe(username)
 
@@ -47,29 +50,31 @@ export async function subscribe(req, res) {
 
 export async function unsubscribe(req, res) {
     const peer = client.peer
+
+    const { username } = req.body
     
+    // Validation
     if (peer.status !== "online")
-        return res.status(404).json({ "message": "You are not online" })
+        return res.status(404).json({ "error": "You are not online" })
 
     if (username === undefined)
         return res.status(400).json({ "error": "Username not provided" })
+    
+    // TODO: check if user is in the network (works if offline, doesn't work if inexistent)
 
-    const { username } = req.body
-
-    // todo: missing validation
     await peer.unsubscribe(username)
 
-    return res.status(200).json({ "message": "Unfollowed user"})
+    return res.status(200).json({ "message": "Unfollowed user" })
 }
 
 export async function post(req, res) {
     const peer = client.peer
 
-    // todo: missing validation
     const { message } = req.body
+    
+    // Validation
     if (message === undefined)
         return res.status(400).json({ "error": "Message not provided" })
-
 
     await peer.send(message)
 
--- modules/src/model/peer/index.js
+++ modules/src/model/peer/index.js
@@ -8,7 +8,7 @@ class Peer {
   constructor(username) {
     this.status = 'offline'
     this.username = username
-    this.subscribedUsers = [] // todo: optimize
+    this.followedUsers = [] // TODO: optimize
   }
 
   getStatus() {
@@ -16,6 +16,7 @@ class Peer {
   }
 
   async start(multiaddr) {
+    // Peer is already online
     if (this.status === 'online'){
       console.log("Peer is already online")
       return
@@ -27,46 +28,54 @@ class Peer {
 
     this.status = 'online'
 
-    // peer is invited to the network
+    // Peer is invited to the network
     if (multiaddr)
       await this.connect(multiaddr)
 
-    // prints this peer's addresses
+    // Prints this peer's addresses
     this.peer.multiaddrs.forEach((ma) => console.log(`${ma.toString()}/p2p/${this.peer.peerId.toB58String()}`))
   }
 
   async connect(multiaddr) {
+    // Connects to peer that invited
     const conn = await this.peer.dial(multiaddr)
 
     console.log(`Connected to ${conn.remotePeer.toB58String()}`)
   }
 
   async subscribe(username) {
-    if (this.subscribedUsers.includes(username))
+    // Assures idempotent subscribe
+    if (this.followedUsers.includes(username))
       return
 
-    this.peer.pubsub.on(username, (msg) => {
-      // missing handler
-      // idea: create a dispatcher, send this message to the dispatcher and dispatcher provides a websocket to communicate with clients
-      console.log(`User ${username} posted ${uint8ArrayToString(msg.data)}`)
+    // Adds listener
+    this.peer.pubsub.on(username, (post) => {
+      // TODO: save post
+      // Idea: create a dispatcher, send this message to the dispatcher and dispatcher provides a websocket to communicate with clients
+      console.log(`User ${username} posted ${uint8ArrayToString(post.data)}`)
     })
 
+    // Adds to followed to users
+    this.followedUsers.push(username)
+    this.followedUsers.forEach((item) => console.log(item))
+
     this.peer.pubsub.subscribe(username)
-    this.subscribedUsers.push(username)
-    this.subscribedUsers.forEach((item) => console.log(item))
     
     console.log(`User ${this.username} followed user ${username}`)
   }
 
   async unsubscribe(username) {
-    // this.peer.pubsub.removeListener(username, this.handler)
+    // Verifies if user is subscribed to the user that he wants to unsubscribe
+    if (!this.followedUsers.includes(username))
+      return
+
     this.peer.pubsub.unsubscribe(username)
     
     console.log(`User ${this.username} unfollowed user ${username}`)
   }
 
   async send(data) {
-    // todo: think about this what should the channel be?
+    // TODO: think about this what should the channel be?
     await this.peer.pubsub.publish(this.username, uint8ArrayFromString(data))
 
     console.log(`User ${this.username} published message ${data}`)

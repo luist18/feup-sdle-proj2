a28f0d6301deb7bd829516aa79070cdf95e5b14f
Ricardo Nunes <ricardo.nunes.1892@gmail.com>
2022-01-04 14:33:46 -0100

    Corrected overwrite

--- modules/src/routes/peer.js
+++ modules/src/routes/peer.js
@@ -1,11 +1,11 @@
 import express from 'express'
 const router = express.Router()
 
-import { start, online, stop, subscribe, unsubscribe, post } from '../controller/peer.controller.js'
+import { start, status, stop, subscribe, unsubscribe, post } from '../controller/peer.controller.js'
 
 // todo: start/stop/subscribe/unsubscribe could be a put
 
-router.get("/online", online)
+router.get("/status", status)
 router.post("/start", start)
 router.post("/stop", stop)
 router.post("/subscribe", subscribe)
